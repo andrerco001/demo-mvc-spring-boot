0e64fef56101263cd141ef58be7dcbaa141aa0f8
Andre <andreoliveira.adv@hotmail.com>
2019-12-03 23:03:53 -0500

    Creation of system controllers

---
+++ demo-mvc-spring-boot/src/main/java/ca/arco/curso/spring/DemoMvcSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ca.arco.curso.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcSpringBootApplication.class, args);
+	}
+
+}
